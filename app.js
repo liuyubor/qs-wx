25b6e54e66b8bd82ebc07f3b6a88921f4536fbfe
Yubo Liu <liuyubor@gmail.com>
2023-03-12 00:07:27 +0800

    加入vant组件，index页加载完成

--- app.js
+++ app.js
@@ -9,11 +9,49 @@ App({
     // 登录
     wx.login({
       success: res => {
+          console.log(res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+            // 将code发送给服务器，由服务器换取用户信息等
+            wx.request({
+              url: `${this.globalData.baseUrl}user/code2uuid`,
+              method: 'POST',
+              data: {code: res.code},
+              success: (response) => {
+                console.log(response);
+                if(response.statusCode === 200) {
+                  // 获取用户信息并进行相关处理
+                  console.log(response.data.openid);
+                  console.log(response.data.session_key);
+                  wx.request({
+                    // 如果用户有userID则登录
+                    url: `${this.globalData.baseUrl}user/wechatLogin`,
+                    data: {
+                      code: response.data.openid
+                    },
+                    method: 'POST',
+                    success: (response) => {
+                      //userID、token、permissions、result
+                      if(response.data.result === 'true') {
+                        console.log("token",response.data.token)
+                        wx.setStorageSync('token', response.data.token);
+                      }
+                    }
+                  })
+                }
+              },
+              fail: function(error) {
+                console.log(error);
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
       }
     })
   },
   globalData: {
+    baseUrl: "http://localhost:8080/qs/",
     userInfo: null,
     active: 0,
     tabbar: [
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,11 @@ const app = getApp()
 Page({
     data: {
         active: 0,
-        motto: 'Hello World',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        canIUseGetUserProfile: false,
-        canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+        isAppointment: false,
+        currentPage: 1,
+        size: 10,
+        hasMore: true,
+        sites: []
     },
     // tabbar处理函数
     onChange(event) {
@@ -18,11 +17,36 @@ Page({
             url: app.globalData.tabbar[event.detail],
         })
     },
-    // 事件处理函数
-    bindViewTap() {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
+    loadMore() {
+        wx.showLoading({
+            title: '加载中...',
+        });
+        wx.request({
+            url: `${app.globalData.baseUrl}site/searchSiteByPage`,
+            method: 'POST',
+            data: {
+                currentPage: this.data.currentPage,
+                size: 10,
+            },
+            success: (res) => {
+                console.log(res);
+                const sites = res.data.sites;
+                if (sites.length < this.data.size) {
+                    console.log(sites.length);
+                    this.setData({
+                        hasMore: false
+                    });
+                }
+                this.setData({
+                    sites: this.data.sites.concat(sites),
+                    currentPage: this.data.currentPage + 1
+                });
+            },
+            complete: () => {
+                wx.hideLoading();
+                console.log(this.data);
+            }
+        });
     },
     onLoad() {
         if (wx.getUserProfile) {
@@ -30,26 +54,12 @@ Page({
                 canIUseGetUserProfile: true
             })
         }
+        this.loadMore();
     },
-    getUserProfile(e) {
-        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-        wx.getUserProfile({
-            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-            success: (res) => {
-                console.log(res)
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
-        })
+    onReachBottom() {
+        if (this.data.hasMore) {
+            this.loadMore();
+        }
     },
-    getUserInfo(e) {
-        // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-        console.log(e)
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
-        })
-    }
+
 })
