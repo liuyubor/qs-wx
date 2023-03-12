7968564a307b9ca7ca77808a7e8ae612d50ac9ff
liuyubor <liuyubor@gmail.com>
2023-03-12 16:09:45 +0800

    updata user.wxss

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,15 +1,15 @@
 // pages/user/user.js
 const app = getApp()
 
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
 Page({
 
     data: {
+        isLogin: true,
         active: 2,
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        canIUseGetUserProfile: false,
-        canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+        avatarUrl: defaultAvatarUrl,
+        username: '注册登录'
     },
       // tabbar处理函数
     onChange(event) {
@@ -21,27 +21,6 @@ Page({
     bindViewTap() {
         wx.navigateTo({
             url: '../logs/logs'
-        })
-    },
-    getUserProfile(e) {
-        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-        wx.getUserProfile({
-            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-            success: (res) => {
-                console.log(res)
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
-        })
-    },
-    getUserInfo(e) {
-        // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-        console.log(e)
-        this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
         })
     },
     onLoad(options) {
