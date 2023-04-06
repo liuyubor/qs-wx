516dd6bdc7d2cef98b87e06cdd0c66f23e656ef8
liuyubor <liuyubor@gmail.com>
2023-04-06 21:09:50 +0800

    完成 success 页

--- pages/index/success/success.js
+++ pages/index/success/success.js
@@ -2,22 +2,48 @@
 
 const app = getApp();
 Page({
-
-    
     data: {
         siteTime: "",
         siteName: "",
         username: "",
         phone: "",
+        status: ""
     },
-    onload: function (options) {
-        console.log(options);
-        this.setData({
-            siteTime: options.time,
-            siteName: options.siteName,
-            username: options.username,
-            phone: options.phone,
-        });
+    /**
+     *  phone: "15581027312"
+        siteId: "2"
+        siteName: "紫金学院图书馆C"
+        time: "17:00-18:00"
+        userId: "o0urj5B04KM8Yw2Gme94keaAA5iU"
+        username: "刘玉博"
+     */
+    onLoad: function (options) {
+        // 从服务器查询预约数据
+        wx.request({
+            url: `${app.globalData.baseUrl}reserve/selectReserveByUserId`,
+            method: 'POST',
+            data: {
+                userId: wx.getStorageSync('openId')
+            },
+            success: (res) => {
+                console.log(res);
+                if (res.statusCode === 200) {
+                    this.setData({
+                        siteTime: res.data.map.time,
+                        siteName: res.data.map.site,
+                        username: res.data.map.user,
+                        phone: wx.getStorageSync('tel'),
+                        status: res.data.map.status
+                    });
+                }
+            }
+        })
+    },
+    // 返回首页
+    onClick: function () {
+        wx.switchTab({
+            url: '/pages/index/index',
+        })
     }
 
 })
