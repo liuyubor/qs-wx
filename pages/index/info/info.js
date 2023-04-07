3f3a2885af7c0509ef883f44a21a9dc0cc73f7a8
liuyubor <liuyubor@gmail.com>
2023-04-07 11:56:00 +0800

    接口调用完善

--- pages/index/info/info.js
+++ pages/index/info/info.js
@@ -8,17 +8,52 @@ Page({
         siteName: "",
         siteDesc: "",
         timeList: null,
-        stats: 69,
+        stats: 0,
+        count: 0,
+        capacity: 0,
+        status: "",
+        dequecount: 0,
     },
-    onLoad: function(options) {
+    onLoad: function (options) {
+        // 获取场地信息
         this.setData({
             siteId: options.id,
             siteName: options.name,
             siteDesc: options.desc,
         });
+        // 获取预约剩余量
+        wx.request({
+            url: `${app.globalData.baseUrl}site/searchSiteInfoById`,
+            method: 'POST',
+            data: {
+                id: options.id
+            },
+            success: res => {
+                console.log(res.data);
+                this.setData({
+                    count: res.data.site.count,
+                    capacity: res.data.site.capacity,
+                    stats: res.data.site.count / res.data.site.capacity * 100,
+                    status: res.data.site.status
+                });
 
-        console.log(this.data);
-
+            }
+        })
+        // 获取排队人数
+        // {"person_num":25,"log_id":1644180873944825103}
+        wx.request({
+            url: `${app.globalData.baseUrl}site/searchDequeueCountById`,
+            method: 'POST',
+            data: {
+                id: options.id
+            },
+            success: res => {
+                console.log(res.data.result);
+                this.setData({
+                    dequecount: res.data.result.person_num
+                });
+            }
+        })
         // 获取时间段信息
         wx.request({
             url: `${app.globalData.baseUrl}site/searchTimeById`,
